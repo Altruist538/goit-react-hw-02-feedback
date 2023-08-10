307a5d67ac539c4987d273036481e92d97d1a82a
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-08-10 22:55:53 +0300

    шаг-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { QuizCounter } from './QuizCounter/QuizCounter';
+export class App extends Component {
+  state = {
+    good: 2,
+    neutral: 3,
+    bad: 4,
+  };
+  changeCounter = option => {
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+    console.log(this.state);
+  };
+
+  render() {
+    let stateNew = this.state;
+    console.log(stateNew);
+    return <QuizCounter item={stateNew} counter={this.changeCounter} />;
+  }
+}
